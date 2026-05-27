22f7367834339f0d9b42b04394cf79bbb7d66ad4
Ananth-Joshi <ananthj82@gmail.com>
2026-05-28 00:38:57 +0530

    modified telemetryengine class

--- Car/app/src/main/java/com/example/phonerover_car/TelemetryEngine.kt
+++ Car/app/src/main/java/com/example/phonerover_car/TelemetryEngine.kt
@@ -1,19 +1,34 @@
 package com.example.phonerover_car
 
 import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
-class TelemetryEngine {
-    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
-    private lateinit var sensorManager: SensorManager
+class TelemetryEngine (context: Context){
+    private var fusedLocationProviderClient: FusedLocationProviderClient
+    private var sensorManager: SensorManager
 
-    private var currentLat = 0.0f;
-    private var currentLng = 0.0f;
-    private var currentHeading = 0.0f;
+    private var currentLat = 0.0;
+    private var currentLng = 0.0;
+    private var currentHeading = 0.0;
 
-    public fun startTracking(context: Context,){
+    init{
+        sensorManager = context.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(context)
+    }
 
+    public fun startTracking(){
+        val rotationVectorSensor = sensorManager.getDefaultSensor(android.hardware.Sensor.TYPE_ROTATION_VECTOR)
+        val sensorEventListener = object: SensorEventListener{
+            override fun onAccuracyChanged(sensor: android.hardware.Sensor?, accuracy: Int){}
+
+            override fun onSensorChanged(event: android.hardware.SensorEvent?) {
+                
+            }
+        }
     }
 
 }
